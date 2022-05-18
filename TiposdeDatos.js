1ea87c142e394b92ac00b7f7ee8ba65bf95558d3
EdwinFV-DSM <al222110833@gmail.com>
2022-05-17 22:50:42 -0500

    Curso de js

---
+++ TiposdeDatos.js
@@ -0,0 +1,32 @@
+//Definimos los tipos de datos
+
+String, cadena="tipo";
+
+int, entero = 100;
+
+true, false = verdad;
+
+null, undefined
+
+Object
+
+Symbol
+
+Boolean
+
+/**
+ * typeof es un tipo de operardor unitario "unytari operator" que trabaja con un operador (variable)
+ * 
+ */
+
+
+typeof 30;
+
+typeof "tipo";
+
+typeof null;
+
+typeof {};
+
+Symbol[s];
+
---
+++ Variables.js
@@ -0,0 +1,32 @@
+/**
+ * Variable es un lugar en memori, Es una presentacion de algun lugar en la memoria para guardar datos
+ */
+
+/**
+ * Variables de tipo cadena y su tipo
+ */
+
+//inicializar
+var nombre;
+
+//inicializar variable
+var nombre = "Diego";
+
+var numero = 25;
+
+/**
+ * La variable nombre, numero apuntara o se guardara en algun lugar de la memoria
+ */
+
+//variables de tipo objeto
+
+var alumnos = ['Computadora','Celular','Audifonos'];
+
+var person= {
+    nombre: "Diego",
+    edad:30
+}
+
+//debugear con js con el parametro con un console.log
+
+console.log(person);
